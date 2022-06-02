5bd06ab8febdf465c6ee75a4ab752edd4bff404e
chinavampire <106727171+chinavampire@users.noreply.github.com>
2022-06-02 14:47:19 +0800

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,10 @@
+﻿<?php
+$conn = @mysqli_connect("ip","user","password","database");
+if (!$conn){
+	die("database connected error:" . mysql_error());
+}
+ini_set("error_reporting","E_ALL & ~E_NOTICE");
+mysqli_query($conn,"set character set 'utf8'");
+mysqli_query($conn,"set names 'utf8'");
+header("Content-type: text/html; charset=utf-8");
+?>
